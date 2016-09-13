abb22f493a6bf39a2c50d26484d8e706a02fc8c5
kassanj <kassandram022@gmail.com>
2016-09-12 21:49:16 -0400

    attempt to resolve group-type display

--- cmb2-group-conditionals.js
+++ cmb2-group-conditionals.js
@@ -1,126 +1,40 @@
 jQuery( document ).ready(function($) {
+   $('input[type="radio"]').click(function() {
+       if($(this).attr('value') == 'link-list') {
+            $('#_wbu_post_styles_list_group_repeat').show();
+            $('.cmb2-id--wbu-post-styles-list-group').show();
 
+       }
 
-	if ($('input#_wbu_post_styles_style6').is('checked')) {
-            document.getElementById('_wbu_post_styles_list_group_repeat').style.display = 'block';
-            document.querySelector('.cmb-repeat-group-wrap').style.display = 'block';
-        }
-    else {
-		document.getElementById('_wbu_post_styles_list_group_repeat').style.display = 'none';
-    	document.querySelector('.cmb-repeat-group-wrap').style.display = 'none';			
-	};
+       else {
+            $('#_wbu_post_styles_list_group_repeat').hide(); 
+            $('.cmb2-id--wbu-post-styles-list-group').hide();    
+       }
+   });
 
-    if ($('input#_wbu_post_styles_style11').is('checked')) {
-        document.getElementById('_wbu_post_styles_education_list_group_repeat').style.display = 'block';
-        document.querySelector('.cmb2-id--wbu-post-styles-education-list-group').style.display = 'block';
-        document.getElementById('_wbu_post_styles_experience').style.display = 'block';
-        document.querySelector('.cmb2-id--wbu-post-styles-experience').style.display = 'block';
-        }
-    else {
-        document.getElementById('_wbu_post_styles_education_list_group_repeat').style.display = 'none';
-        document.querySelector('.cmb2-id--wbu-post-styles-education-list-group').style.display = 'none';
-        document.getElementById('_wbu_post_styles_experience').style.display = 'none';
-        document.querySelector('.cmb2-id--wbu-post-styles-experience').style.display = 'none';             
-    };
- 
-    if ($('input#_wbu_post_styles_style12').prop('checked', true)) {
-        document.getElementById('_wbu_post_styles_political_race_group_repeat').style.display = 'block';
-        document.querySelector('.cmb2-id--wbu-post-styles-political-race-group').style.display = 'block';
-        }
-    else {
-        document.getElementById('_wbu_post_styles_political_race_group_repeat').style.display = 'none';
-        document.querySelector('.cmb2-id--wbu-post-styles-political-race-group').style.display = 'none';            
-    };
+    $('input[type="radio"]').click(function() {
+       if($(this).attr('value') == 'single-voter-guide') {
+            $('#_wbu_post_styles_education_list_group_repeat').show(); 
+            $('.cmb2-id--wbu-post-styles-education-list-group').show();            
+       }
 
+       else {
+            $('#_wbu_post_styles_education_list_group_repeat').hide();
+            $('.cmb2-id--wbu-post-styles-education-list-group').hide();        
+       }
+   });
 
-    $('input#_wbu_post_styles_style6').change(function() {
-        if ($(this).prop('checked', true)) {
-            document.getElementById('_wbu_post_styles_list_group_repeat').style.display = 'block';
-            document.querySelector('.cmb-repeat-group-wrap').style.display = 'block';
-        }
-    });
+    $('input[type="radio"]').click(function() {
+       if($(this).attr('value') == 'main-voter-guide') {
+            $('#_wbu_post_styles_political_race_group_repeat').show(); 
+            $('.cmb2-id--wbu-post-styles-political-race-group').show();            
+       }
 
-    $('input#_wbu_post_styles_style11').change(function() {
-        if ($(this).prop('checked', true)) {
-            document.getElementById('_wbu_post_styles_education_list_group_repeat').style.display = 'block';
-            document.querySelector('.cmb2-id--wbu-post-styles-education-list-group').style.display = 'block';
-            document.getElementById('_wbu_post_styles_experience').style.display = 'block';
-            document.querySelector('.cmb2-id--wbu-post-styles-experience').style.display = 'block';         
-        }
-    });
+       else {
+            $('#_wbu_post_styles_political_race_group_repeat').hide();
+            $('.cmb2-id--wbu-post-styles-political-race-group').hide();        
+       }
+   });
+    
 
-    $('input#_wbu_post_styles_style12').change(function() {
-        if ($(this).prop('checked', true)) {
-            document.getElementById('_wbu_post_styles_political_race_group_repeat').style.display = 'block';
-            document.querySelector('.cmb2-id--wbu-post-styles-political-race-group').style.display = 'block';         
-        }
-    });
-
-    $('input#_wbu_post_styles_style1').change(function() {
-        if ($(this).prop('checked', true)) {
-            document.getElementById('_wbu_post_styles_list_group_repeat').style.display = 'none';
-            document.querySelector('.cmb-repeat-group-wrap').style.display = 'none';
-        }
-    });
-
-    $('input#_wbu_post_styles_style2').change(function() {
-        if ($(this).prop('checked', true)) {
-            document.getElementById('_wbu_post_styles_list_group_repeat').style.display = 'none';
-            document.querySelector('.cmb-repeat-group-wrap').style.display = 'none';
-        }
-    });
-
-    $('input#_wbu_post_styles_style3').change(function() {
-        if ($(this).prop('checked', true)) {
-            document.getElementById('_wbu_post_styles_list_group_repeat').style.display = 'none';
-            document.querySelector('.cmb-repeat-group-wrap').style.display = 'none';
-        }
-    });
-
-    $('input#_wbu_post_styles_style4').change(function() {
-        if ($(this).prop('checked', true)) {
-            document.getElementById('_wbu_post_styles_list_group_repeat').style.display = 'none';
-            document.querySelector('.cmb-repeat-group-wrap').style.display = 'none';
-        }
-    });
-
-    $('input#_wbu_post_styles_style5').change(function() {
-        if ($(this).prop('checked', true)) {
-            document.getElementById('_wbu_post_styles_list_group_repeat').style.display = 'none';
-            document.querySelector('.cmb-repeat-group-wrap').style.display = 'none';
-        }
-    });
-
-    $('input#_wbu_post_styles_style7').change(function() {
-        if ($(this).prop('checked', true)) {
-            document.getElementById('_wbu_post_styles_list_group_repeat').style.display = 'none';
-            document.querySelector('.cmb-repeat-group-wrap').style.display = 'none';
-        }
-    });
-
-    $('input#_wbu_post_styles_style8').change(function() {
-        if ($(this).prop('checked', true)) {
-            document.getElementById('list_group_repeat').style.display = 'none';
-            document.querySelector('.cmb-repeat-group-wrap').style.display = 'none';
-        }
-    });
-
-    $('input#_wbu_post_styles_style9').change(function() {
-        if ($(this).prop('checked', true)) {
-            document.getElementById('list_group_repeat').style.display = 'none';
-            document.querySelector('.cmb-repeat-group-wrap').style.display = 'none';
-        }
-    });
-    $('input#_wbu_post_styles_style10').change(function() {
-        if ($(this).prop('checked', true)) {
-            document.getElementById('list_group_repeat').style.display = 'none';
-            document.querySelector('.cmb-repeat-group-wrap').style.display = 'none';
-        }
-    });
-    $('input#_wbu_post_styles_style11').change(function() {
-        if ($(this).prop('checked', true)) {
-            document.getElementById('list_group_repeat').style.display = 'none';
-            document.querySelector('.cmb-repeat-group-wrap').style.display = 'none';
-        }
-    });
 });
